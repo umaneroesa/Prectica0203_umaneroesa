f8cd1998668d12ea0526e477a912b9d1ead5a273
umaneroesa <umaneroesa1@educacion.navarra.es>
2024-10-25 08:25:21 +0200

    Fin11

---
+++ Ejercicio11.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
+palabra= str(input("Dime una palabra "))
+for i in range(len(palabra)-1, -1, -1):
+    print(palabra[i])
